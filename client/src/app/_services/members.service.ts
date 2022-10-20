81129dee8647054227d4a51a238635d25826bb74
rick-guiz <rick.guiz@gmail.com>
2022-10-20 13:46:58 +0800

    End of section 10b

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
-import { map, of } from 'rxjs';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
-  member: any;
   members: Member[] = [];
 
-    
   constructor(private http: HttpClient) { }
 
   getMembers() {
@@ -23,25 +22,20 @@ export class MembersService {
         return members;
       })
     )
-
   }
 
   getMember(username: string) {
     const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
-
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
-        const index = this.member.indexOf(member);
-        this.member[index] = member;
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
       })
     )
   }
-
-
-
 }
