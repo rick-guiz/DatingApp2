42df0a30e1d7a57b7c4798073140c8b91d35f1b0
rick-guiz <rick.guiz@gmail.com>
2022-10-28 16:36:29 +0800

    End of  Section 12 L.140

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -52,6 +52,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxGalleryModule,
     NgxSpinnerModule
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -10,22 +11,35 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
+
 
   model: any = {};
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.intitializeForm();
   }
 
-  register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
+  intitializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(), 
+      confirmPassword: new FormControl(),
     })
+  }
+
+
+  register() {
+    console.log(this.registerForm.value)
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // })
   }
 
 
