3097ea1f5aed47f82dd01a6ef89820758cf1f2b1
rick-guiz <rick.guiz@gmail.com>
2022-10-31 15:07:36 +0800

    End of section 12.L148

--- API/Helpers/ AutoMapperProfiles.cs
+++ API/Helpers/ AutoMapperProfiles.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
-using AutoMapper;
 using API.Extensions;
-
-
+using AutoMapper;
 
 namespace API.Helpers
 {
-    public class  AutoMapperProfiles: Profile
+    public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
@@ -19,10 +14,9 @@ namespace API.Helpers
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => 
                     src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
-                    
             CreateMap<Photo, PhotoDto>();
-            
             CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
