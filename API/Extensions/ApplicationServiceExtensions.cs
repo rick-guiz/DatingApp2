40625229d289fe9a8b8b6cc4be703a563dee06c5
rick-guiz <rick.guiz@gmail.com>
2022-11-06 22:50:51 +0800

    End of section 13.L162

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,8 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             
             services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddScoped<LogUserActivity>();
             
             services.AddScoped<IUserRepository, UserRepository>();
 
