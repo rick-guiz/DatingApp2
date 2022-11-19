09ac156398a15b714e3c74587c7f89e76ff38e02
rick-guiz <rick.guiz@gmail.com>
2022-11-19 22:15:05 +0800

    End of section 16

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -8,15 +8,18 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
-  title: string;
-  list: any[];
-  closeBtnName: string;
+  @Input() updateSelectedRoles = new EventEmitter();
+  user: User;
+  roles: any[]; 
 
   constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
   }
 
-  
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
+  }
 
 }
