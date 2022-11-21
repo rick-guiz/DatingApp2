21c712c68b05deda337da1feb98b758fb5668ae9
rick-guiz <rick.guiz@gmail.com>
2022-11-21 20:11:50 +0800

    End of section 17.L222

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
+using API.SignalR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,7 +23,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace API
 {
-    public class Startup
+   public class Startup
     {
         private readonly IConfiguration _config;
 
@@ -38,6 +39,7 @@ namespace API
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(_config);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,8 +56,11 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
+                
             app.UseAuthentication();
             
             app.UseAuthorization();
@@ -63,6 +68,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PresenceHub>("hubs/presence");
             });
         }
     }
