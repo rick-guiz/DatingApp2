8c8fe4d1c46dd4837bdaab3ae49fcfd07060623f
rick-guiz <rick.guiz@gmail.com>
2022-12-06 13:33:12 +0800

    End of section 17.L234

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -16,8 +16,9 @@ namespace API.SignalR
         }
         public override async Task OnConnectedAsync()
         {
-            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+             var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+            if (isOnline)
+                await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
 
             var currentUsers = await _tracker.GetOnlineUsers();
             await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
@@ -25,12 +26,11 @@ namespace API.SignalR
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
-
-            var currentUsers = await _tracker.GetOnlineUsers();
-            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
+           var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             
+            if (isOffline)
+                await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
             await base.OnDisconnectedAsync(exception);
         }
     }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
-import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
-import { take } from 'rxjs';
 import { Member } from 'src/app/_models/member';
-import { Message } from 'src/app/_models/message';
-import { User } from 'src/app/_models/user';
-import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 import { PresenceService } from 'src/app/_services/presence.service';
-
+import { AccountService } from 'src/app/_services/account.service';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-
 export class MemberDetailComponent implements OnInit, OnDestroy {
   @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   member: Member;
@@ -25,19 +23,14 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   galleryImages: NgxGalleryImage[];
   activeTab: TabDirective;
   messages: Message[] = [];
-  user?: User;
-  
+  user: User;
+
   constructor(public presence: PresenceService, private route: ActivatedRoute, 
-              private messageService: MessageService, private accountService: AccountService,
-              private router: Router) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-    next: user => {
-      if (user) 
-        this.user = user;
-      }
-    });
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-  }
+    private messageService: MessageService, private accountService: AccountService,
+    private router: Router) { 
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
@@ -62,10 +55,6 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
     this.galleryImages = this.getImages();
   }
 
-  ngOnDestroy(): void {
-    this.messageService.stopHubConnection();
-  }
-  
   getImages(): NgxGalleryImage[] {
     const imageUrls = [];
     for (const photo of this.member.photos) {
@@ -87,14 +76,18 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   selectTab(tabId: number) {
     this.memberTabs.tabs[tabId].active = true;
   }
-  
+
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
-    if (this.activeTab.heading === 'Messages' && this.user) {
+    if (this.activeTab.heading === 'Messages' && this.messages.length === 0) {
       this.messageService.createHubConnection(this.user, this.member.username);
-    } 
-    else {
+    } else {
       this.messageService.stopHubConnection();
     }
   }
+
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
+  }
+
 }
